676920cb473418986e19d0fdf4c08c24a4e0da5b
Aman <aman.swar001@gmail.com>
2025-12-30 17:02:05 +0530

    add todo

---
+++ ds_algo/ds/linked_list.py
@@ -0,0 +1,17 @@
+"""
+To Do 
+- Single LinkedList
+- double LinkedList
+- Circular LinkedList
+
+functions
+- reverse
+- sort
+- find
+- append
+- delete
+- merge + merge k sorted list
+- count
+- copy
+
+"""
