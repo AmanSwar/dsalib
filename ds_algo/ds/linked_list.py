8a341e7490d319545bf5f8e778f60db081e5561d
Aman <aman.swar001@gmail.com>
2026-01-14 16:11:11 +0530

    feat : add linked list

--- ds_algo/ds/linked_list.py
+++ ds_algo/ds/linked_list.py
@@ -15,3 +15,53 @@ functions
 - copy
 
 """
+
+numerics = int | float
+
+
+class Node:  
+  def __init__(self , value : numerics , next = None):
+
+    self.value = value
+    self.next = next
+  
+
+
+class LinkedList:
+
+  def __init__(self , array : list[numerics]):
+
+    self.head = Node(value=-1 , next=None)
+    self.__init(array)
+    
+  def __init(self , array):
+
+    first_node = Node(array[0] , None)
+    self.head.next = first_node
+
+    current_node = first_node
+
+    for i in range(1 , len(array)):
+
+      new_node = Node(array[i] , None)
+      current_node.next = new_node
+
+      current_node = new_node
+
+  def printLL(self):
+
+    curr_node : Node = self.head.next
+    while(curr_node):
+      print(curr_node.value , end=" -> ")
+      curr_node = curr_node.next
+
+    print("None")
+
+
+if __name__ == "__main__":
+
+  array = [i for i in range(0,10)]
+  ll = LinkedList(array)
+
+  ll.printLL()
+
